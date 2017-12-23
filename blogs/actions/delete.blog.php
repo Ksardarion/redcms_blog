b54b228f53a6c1526812afe8ff36839b26af1ba9
Andriy Yanitskii <andriy.yanitskii@goodahead.com>
2017-12-24 01:37:59 +0200

    Microfix for return link

--- blogs/actions/delete.blog.php
+++ blogs/actions/delete.blog.php
@@ -7,6 +7,7 @@
  */
 require_once '../../sys/inc/start.php';
 require_once '../../sys/plugins/classes/blogs.class.php';
+require_once '../../sys/plugins/classes/blogs.comments.class.php';
 
 $doc = new document(1);
 if (!isset($_GET ['id']) || !is_numeric($_GET ['id'])) {
@@ -31,10 +32,13 @@ if(!$user->access('blogs_delete_blog') && $blog['author'] !== $user->id) {
     $doc->access_denied(__('У Вас нет доступа!'));
 }
 
-blogs::deleteBlog($blog['id']);
-$doc->msg(__('Блог успешно удален'));
+$res = blogs::deleteBlog($blog['id']);
+if($res)
+    $doc->msg(__('Блог успешно удален'));
+else
+    $doc->err(__('Что-то случилось, и блог удалился не полностью'));
 
 if (isset($_GET ['return']))
     $doc->ret(__('Вернуться'), text::toValue($_GET ['return']));
 else
-    $doc->ret(__('Вернуться'), './');
+    $doc->ret(__('Вернуться'), '../category.php?id=' . $blog['category']);
